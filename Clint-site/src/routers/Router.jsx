bae17f3f476cc93a86c9ad2a0e65e1a3250c3d05
Anisur369 <anisurrahman57136@gmail.com>
2026-01-20 19:59:55 +0600

    config update

--- Clint-site/src/routers/Router.jsx
+++ Clint-site/src/routers/Router.jsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
-import Home from "../pages/home/Home.jsx";
-import About from "../pages/About/About.jsx";
-import Contact from "../pages/Contact/Contact.jsx";
-import Portfolio from "../pages/Portfolio/Portfolio.jsx";
-import NotFound from "../pages/NotFound/NotFound.jsx";
-import { Component } from "react";
+import {createBrowserRouter} from "react-router";
 import RootLayout from "../layout/RootLayout.jsx";
+import Home from "../pages/home/Home.jsx";
+// import About from "../pages/About/About.jsx";
+// import Contact from "../pages/Contact/Contact.jsx";
+// import Portfolio from "../pages/Portfolio/Portfolio.jsx";
+// import NotFound from "../pages/NotFound/NotFound.jsx";
+// import { Component } from "react";
 
 export const router = createBrowserRouter([
     {
@@ -14,20 +14,9 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: "/",
+                index: true,
                 Component: Home
             },
-            {
-                path: "/about",
-                Component: About
-            },
-            {
-                path: "/contact",
-                Component: Contact
-            },
-            {
-                path: "/portfolio",
-                Component: Portfolio
-            }
         ]
     },
     {
